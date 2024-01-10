db9e9713e5dc4077ccc7e8d24d69abc31033aad5
Milman <milindmjoshi@live.com>
2024-01-10 14:49:19 -0500

    Working random password generator, cleaned up code and simplified

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var alphaUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var specialChar = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|","\\",":",";","<",",",">",".","?","/","]"];
-var numericChar = [0,1,2,3,4,5,6,7,8,9];
+var alphaUpperArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var alphaLowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialCharArray = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|","\\",":",";","<",",",">",".","?","/","]"];
+var numericCharArray = [0,1,2,3,4,5,6,7,8,9];
+var passwordCharArray;
 
 // Write password to the #password input
 function writePassword() {
@@ -15,38 +16,11 @@ function writePassword() {
 
 }
 
-var rau = function randomAlphaUpper(){
-  var char = alphaUpper[Math.floor(Math.random()*alphaUpper.length)];
-  console.log("Random alpha upper: " + char);
+var randomChar = function(){
+  var char = passwordCharArray[Math.floor(Math.random()*passwordCharArray.length)];
+  console.log("Random password char: " + char);
   return char;
 }
-
-var ral = function randomAlphaLower(){
-  var char = alphaLower[Math.floor(Math.random()*alphaLower.length)];
-  console.log("Random alpha lower: " + char);
-  return char;
-}
-
-var rsc = function randomSpecialChar(){
-  var char = specialChar[Math.floor(Math.random()*specialChar.length)];
-  console.log("Random special char: " + char);
-  return char;
-}
-
-var rnc = function randomNumericChar(){
-  var char = numericChar[Math.floor(Math.random()*numericChar.length)];
-  console.log("Random numeric char: " + char);
-  return char;
-}
-
-var functionArray = [rau,ral,rsc,rnc];
-
-function randomCharFunction(){
-  var randomFunction = functionArray[Math.floor(Math.random()*functionArray.length)];
-  console.log("Random function: " + randomFunction.name);
-  return randomFunction();
-}
-
 
 
 
@@ -62,108 +36,55 @@ function generatePassword(){
   }
   console.log("Length:" + length);
 
-  var useLowerCase = confirm("Include lower case characters?");
-  console.log("Include lower case: " + useLowerCase);
-  var useUpperCase = confirm("Include upper case characters?");
-  console.log("Include upper case: " + useUpperCase);
-  var useNumeric = confirm("Include numeric characters?");
-  console.log("Include numeric characters: " + useNumeric);
-  var useSpecialChar = confirm("Include special characters?");
-  console.log("Include special characters: " + useSpecialChar);
+  //reset passwordCharArray to empty Array
+  passwordCharArray = [];
 
-  // init password object 
-  var passwordRules = {
 
-    // Since their are 4 boolean values we can have 2^4 combinations which is 16, but
-    // no selections is not allowed so only 15 combinations 
-    //  useLowerCase  useUpperCase useNumeric useSpecialChar
-    //  useLowerCase  useUpperCase useNumeric !useSpecialChar
-    //  useLowerCase  useUpperCase !useNumeric useSpecialChar
-    //  useLowerCase  useUpperCase !useNumeric !useSpecialChar
-    //  useLowerCase  !useUpperCase useNumeric useSpecialChar
-    //  useLowerCase  !useUpperCase useNumeric !useSpecialChar
-    //  useLowerCase  !useUpperCase !useNumeric useSpecialChar
-    //  useLowerCase  !useUpperCase !useNumeric !useSpecialChar
-    //  !useLowerCase  useUpperCase useNumeric useSpecialChar
-    //  !useLowerCase  useUpperCase useNumeric !useSpecialChar
-    //  !useLowerCase  useUpperCase !useNumeric useSpecialChar
-    //  !useLowerCase  useUpperCase !useNumeric !useSpecialChar
-    //  !useLowerCase  !useUpperCase useNumeric useSpecialChar
-    //  !useLowerCase  !useUpperCase useNumeric !useSpecialChar
-    //  !useLowerCase  !useUpperCase !useNumeric useSpecialChar
-    //  !useLowerCase  !useUpperCase !useNumeric !useSpecialChar (not allowed and causes error)
-    
-    useLowerCase: useLowerCase,
-    useUpperCase: useUpperCase,
-    useNumeric: useNumeric,
-    useSpecialChar: useSpecialChar,
+  var lowerCase = confirm("Include lower case characters?");
+  console.log("Include lower case: " + lowerCase);
+  var upperCase = confirm("Include upper case characters?");
+  console.log("Include upper case: " + upperCase);
+  var numeric = confirm("Include numeric characters?");
+  console.log("Include numeric characters: " + numeric);
+  var specialChar = confirm("Include special characters?");
+  console.log("Include special characters: " + specialChar);
 
-    lowerCaseOnly: function(){
-      if (useLowerCase && (!useUpperCase && !useNumeric && !useSpecialChar)){
-        return true;
-      }
-      return false;
-    },
-    upperCaseOnly: function(){
-      if (useUpperCase && (!useLowerCase && !useNumeric && !useSpecialChar)){
-        return true;
-      }
-      return false;
-    },
-    numericOnly: function(){
-      if (useNumeric && (!useLowerCase && !useUpperCase && !useSpecialChar)){
-        return true;
-      }
-      return false;
-    },
-    specialOnly: function(){
-      if (useSpecialChar && (!useLowerCase && !useUpperCase && !useNumeric)){
-        return true;
-      }
-      return false;
-    }
-  }
-
+   
   //If no type of characters selected , display error message and return
-  if (!useLowerCase && !useUpperCase && !useNumeric && !useSpecialChar){
+  if (!lowerCase && !upperCase && !numeric && !specialChar){
     alert("You must select one type of character to use");
     return;
   }
 
-
-  var password = "";
-  //initialize lowerCaseUsed used to false if  required, else set to true
-  var lowerCaseUsed =  useLowerCase == true?false:true;
-  var upperCaseUsed =  useUpperCase == true?false:true;
-  var specialCharUsed = useSpecialChar == true?false:true;
-  var numericUsed = useNumeric == true?false:true;
-  
-
-  // This doesn't work unless we select all types
-  for (var i=0;i<length;i++){
-
-    if (useLowerCase && !lowerCaseUsed){
-      password += ral();
-      lowerCaseUsed = true;
-    }
-    else if (useUpperCase && !upperCaseUsed){
-      password += rau();
-      upperCaseUsed = true;
-    }
-    else if (useNumeric && !numericUsed){
-      password += rnc();
-      numericUsed = true;
-    }
-    else if (useSpecialChar && !specialCharUsed){
-      password += rsc();
-      specialCharUsed = true;
-    }
-    else{
-      password += randomCharFunction();
-    }
-    console.log("password: " + password);
+  if (lowerCase){
+    passwordCharArray=passwordCharArray.concat(alphaLowerArray);
   }
 
-  // select types of characters
+  if (upperCase){
+    passwordCharArray=passwordCharArray.concat(alphaUpperArray);
+  }
+
+  if (specialChar){
+    passwordCharArray=passwordCharArray.concat(specialCharArray);
+  }
+
+  if (numeric){
+    passwordCharArray=passwordCharArray.concat(numericCharArray);
+  }
+
+  console.log("Password Char Array: " + passwordCharArray);
+
+
+  var password = "";
+  
+
+  for (var i=0;i<length;i++){
+      var char = randomChar();
+      console.log("Random password char: " + char);
+      password += char;
+  }
+    console.log("password: " + password);
+
+  // return password
   return password;
 }
